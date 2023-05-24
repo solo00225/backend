a96de13f74c0089aa77eb97a897e873f208d742f
souleymane1234 <konatesouleymane42@gmail.com>
2023-05-24 21:16:53 +0200

    first commit

---
+++ models/Cinema.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
+const { isResolvable } = require("@hapi/joi/lib/common");
+
+
+const CinemaSchema = new mongoose.Schema({
+  nom: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+
+  description: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: String,
+    required: true
+  },
+  heure: {
+    type: String,
+    required: true
+  },
+  prixStandart: {
+    type: String,
+    required: true
+  },
+  prixVip: {
+    type: String,
+    required: true
+  },
+  nomLieu: {
+    type: String,
+    required: true
+  },
+  descriptionLieu: {
+    type: String,
+    required: true
+  },
+created_at: {
+  type: Date,
+  default: Date.now
+},
+updated_at: {
+  type: Date,
+  default: Date.now
+ }
+});
+
+
+
+module.exports = mongoose.model("Cinema", CinemaSchema);
